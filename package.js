d42e494f78538fe8726e65848450fe824e485b96
Darek <szarebure@o2.pl>
2015-10-06 23:32:46 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'brajt:orion-lang-pl',
+  /**
+   * Try to keep the same version of Orion, so people know
+   * which version is translated or maybe they want to use
+   * a old Orion version.
+   *
+   * If you want to update this package change only the minor
+   * version indicator: 1.3.1, 1.3.2. Always keep the current
+   * orion version (currently 1.3.x).
+   */
+  version: '1.3.0',
+  summary: 'Orion polish language',
+  git: '[git-repository-url]',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use('orionjs:lang-en@1.3.0'); // This is the base language
+  api.imply('orionjs:lang-en');
+
+  api.addFiles('pl.js');
+});
